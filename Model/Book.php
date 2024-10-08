0ecfbce013ff84ccc28e1c513b5f68424ba1798e
Freitas Maq 9 <12202444@aluno.cotemig.com.br>
2024-10-08 10:37:32 -0300

    Definição de classes User e Book e respectivos Dao's

---
+++ Model/Book.php
@@ -0,0 +1,69 @@
+<?php
+class Book {
+    protected $id;
+    protected $nome;
+    protected $autor;
+    protected $genero;
+    protected $estoque;
+    protected $dataEntrada;
+    protected $dataSaida;
+
+    // Getters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getAutor() {
+        return $this->autor;
+    }
+
+    public function getGenero() {
+        return $this->genero;
+    }
+
+    public function getEstoque() {
+        return $this->estoque;
+    }
+
+    public function getDataEntrada() {
+        return $this->dataEntrada;
+    }
+
+    public function getDataSaida() {
+        return $this->dataSaida;
+    }
+
+    // Setters
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function setAutor($autor) {
+        $this->autor = $autor;
+    }
+
+    public function setGenero($genero) {
+        $this->genero = $genero;
+    }
+
+    public function setEstoque($estoque) {
+        $this->estoque = $estoque;
+    }
+
+    public function setDataEntrada($dataEntrada) {
+        $this->dataEntrada = $dataEntrada;
+    }
+
+    public function setDataSaida($dataSaida) {
+        $this->dataEntrada = $dataSaida;
+    }
+}
+?>
---
+++ Model/Dao/BookDaoImpl.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once '../Connection.php';
+require_once 'BookDao.php';
+require_once '../Book.php';
+
+class BookDaoImpl implements BookDao {
+    private $conn;
+
+    public function __construct() {
+        $this->conn = (new Connection())->getConnection();
+    }
+
+    public function getAllBooks() {
+        try {
+            $statement = $this->conn->prepare("SELECT * FROM book");
+            $statement->execute();
+            return $statement->fetchAll(PDO::FETCH_CLASS, 'Book');
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function getBook($id) {
+        try {
+            $statement = $this->conn->prepare("SELECT * FROM book WHERE id = :id");
+            $statement->bindParam(':id', $id);
+            $statement->execute();
+            return $statement->fetchObject('Book');
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function createBook($book) {
+        try {
+            $statement = $this->conn->prepare("INSERT INTO book (nome, autor, genero, estoque, dataEntrada, dataSaida) VALUES (:nome, :autor, :genero, :estoque, :dataEntrada, :dataSaida)");
+            $statement->bindParam(':nome', $book->getTitulo());
+            $statement->bindParam(':autor', $book->getAutor());
+            $statement->bindParam(':genero', $book->getGenero());
+            $statement->bindParam(':estoque', $book->getEstoque());
+            $statement->bindParam(':dataEntrada', $book->getDataEntrada());
+            $statement->bindParam(':dataSaida', $book->getDataSaida());
+            return $statement->execute();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function updateBook($book) {
+        try {
+            $statement = $this->conn->prepare("UPDATE book SET nome = :nome, autor = :autor, genero = :genero, estoque = :estoque, dataEntrada = :dataEntrada, datasaida = :datasaida, dataSaida = :dataSaida WHERE id = :id");
+            $statement->bindParam(':id', $book->getId());
+            $statement->bindParam(':nome', $book->getNome());
+            $statement->bindParam(':autor', $book->getAutor());
+            $statement->bindParam(':genero', $book->getGenero());
+            $statement->bindParam(':estoque', $book->getEstoque());
+            $statement->bindParam(':dataEntrada', $book->getDataEntrada());
+            $statement->bindParam(':dataSaida', $book->getDataSaida());
+            return $statement->execute();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function deleteBook($id) {
+        try {
+            $statement = $this->conn->prepare("DELETE FROM book WHERE id = :id");
+            $statement->bindParam(':id', $id);
+            return $statement->execute();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+}
+?>
