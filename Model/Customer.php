40bbec1466074ddc4e3c3de49d4539dede7cc069
Freitas Maq 9 <12202444@aluno.cotemig.com.br>
2024-10-08 11:05:06 -0300

    Criação da classe Customer e respectivos Dao's

---
+++ Model/Customer.php
@@ -0,0 +1,52 @@
+<?php
+class Customer {
+    protected $id;
+    protected $nome;
+    protected $email;
+    protected $telefone;
+    protected $senha;
+
+    // Getters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getTelefone() {
+        return $this->telefone;
+    }
+
+    public function getSenha() {
+        return $this->senha;
+    }
+
+
+    // Setters
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function settelefone($telefone) {
+        $this->telefone = $telefone;
+    }
+
+    public function setSenha($senha) {
+        $this->senha = $senha;
+    }
+}
+?>
