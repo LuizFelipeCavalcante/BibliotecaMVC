858170266934713dc946e9dbb8dbb80aca07c8f6
Arthur Freitas <12202444@aluno.cotemig.com.br>
2024-10-22 09:54:37 -0300

    customerDao atualização

--- Model/Dao/CustomerDao.php
+++ Model/Dao/CustomerDao.php
@@ -1,9 +1,7 @@
 <?php
 interface CustomerDao {
-    public function getAllCustomers();
     public function getCustomer($id);
     public function createCustomer($customer);
-    public function updateCustomer($customer);
-    public function deleteCustomer($id);
+    public function login($email,$senha);
 }
 ?>
